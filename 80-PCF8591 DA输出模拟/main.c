8a06bf6d49b16b61af7a25889f6adf58fde98ecf
xiaoyaoltian <32162482+xiaoyaoltian@users.noreply.github.com>
2018-08-28 12:19:14 +0800

    PCF8591 DA输出模拟
    PCF8591 DA输出模拟

---
+++ 80-PCF8591 DA输出模拟/main.c
@@ -0,0 +1,59 @@
+﻿/*******************************************************************************
+* 版  权 	: 红梅科技工作室
+* 论  坛	：https://blog.csdn.net/Qingzhusshuiyun
+* Q Q	    ：2369099714
+* 版权所有，盗版必究。
+* 工  程   : DA输出演示
+* 文件名   : DA输出演示.c
+* 处理器   : STC89C52RC
+* 编译环境 : Keil 5
+* 系统时钟 : 12MHZ
+* 版    本	: V1.0 
+* 生成日期	: 2018-07-20	   					
+* 修改日期	:  
+* 简单描述 : 使用DA输入，数码管显示输出数字量，LED显示模拟电压大小
+*******************************************************************************/
+ 
+#include <reg52.h>                
+#include "i2c.h"
+#include "delay.h"
+#include "display.h"
+
+#define AddWr 0x90   //写数据地址 
+#define AddRd 0x91   //读数据地址
+
+extern bit ack;
+bit WriteDAC(unsigned char dat);
+/*------------------------------------------------
+              主程序
+------------------------------------------------*/
+main()
+{
+ unsigned char num=0;
+ Init_Timer0();
+ while (1)         //主循环
+  {
+ WriteDAC(num);
+ num++;//连续累加，值从0-255反复循环，并显示在数码管上，
+ TempData[0]=dofly_DuanMa[num/100];    
+ TempData[1]=dofly_DuanMa[(num%100)/10];
+ TempData[2]=dofly_DuanMa[(num%100)%10];
+ DelayMs(100);
+  }
+}
+
+/*------------------------------------------------
+               写入DA转换数值
+输入参数：dat 表示需要转换的DA数值，范围是0-255
+------------------------------------------------*/
+bit WriteDAC(unsigned char dat)
+{
+   Start_I2c();               //启动总线
+   SendByte(AddWr);             //发送器件地址
+     if(ack==0)return(0);
+   SendByte(0x40);            //发送器件子地址
+     if(ack==0)return(0);
+   SendByte(dat);             //发送数据
+     if(ack==0)return(0);
+   Stop_I2c();  
+}
