e6ff6387033ef1cabf63f73681ff53eb32ec5881
xiaoyaoltian <32162482+xiaoyaoltian@users.noreply.github.com>
2018-09-13 11:24:57 +0800

    1个18b20温度传感器1602液晶显示
    1个18b20温度传感器1602液晶显示

---
+++ 93-1个18b20温度传感器1602液晶显示/1602.h
@@ -0,0 +1,25 @@
+﻿
+#include<reg52.h> //包含头文件，一般情况不需要改动，头文件包含特殊功能寄存器的定义
+#include<intrins.h>
+
+#ifndef __1602_H__
+#define __1602_H__
+
+
+ bit LCD_Check_Busy(void) ;
+
+ void LCD_Write_Com(unsigned char com) ;
+
+ void LCD_Write_Data(unsigned char Data) ;
+
+ void LCD_Clear(void) ;
+
+ void LCD_Write_String(unsigned char x,unsigned char y,unsigned char *s) ;
+
+ void LCD_Write_Char(unsigned char x,unsigned char y,unsigned char Data) ;
+
+ void LCD_Init(void) ;
+
+ void Lcd_User_Chr(void);
+   
+#endif
