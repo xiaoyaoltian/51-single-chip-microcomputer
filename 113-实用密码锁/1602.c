1c621e8dd3b3c198448f62d40cf607313904f285
xiaoyaoltian <32162482+xiaoyaoltian@users.noreply.github.com>
2018-09-17 09:45:16 +0800

    实用密码锁
    实用密码锁

---
+++ 113-实用密码锁/1602.c
@@ -0,0 +1,135 @@
+﻿/*******************************************************************************
+* 版  权 	: 红梅科技工作室
+* 论  坛	：https://blog.csdn.net/Qingzhusshuiyun
+* Q Q	    ：2369099714
+* 版权所有，盗版必究。
+* 工  程   : 实用密码锁
+* 文件名   : 实用密码锁.c
+* 处理器   : STC89C52RC
+* 编译环境 : Keil 5
+* 系统时钟 : 12MHZ
+* 版    本	: V1.0 
+* 生成日期	: 2018-08-22	   					
+* 修改日期	:  
+* 简单描述 : 1-VSS 2-VDD 3-V0 4-RS 5-R/W 6-E 7-14 DB0-DB7 15-BLA 16-BLK
+*******************************************************************************/
+
+#include "1602.h"
+#include "delay.h"
+
+#define CHECK_BUSY
+
+sbit RS = P2^4;   //定义端口 
+sbit RW = P2^5;
+sbit EN = P2^6;
+
+#define RS_CLR RS=0 
+#define RS_SET RS=1
+
+#define RW_CLR RW=0 
+#define RW_SET RW=1 
+
+#define EN_CLR EN=0
+#define EN_SET EN=1
+
+#define DataPort P0
+
+/*------------------------------------------------
+              判忙函数
+------------------------------------------------*/
+ bit LCD_Check_Busy(void) 
+ { 
+#ifdef CHECK_BUSY
+ DataPort= 0xFF; 
+ RS_CLR; 
+ RW_SET; 
+ EN_CLR; 
+ _nop_(); 
+ EN_SET;
+ return (bit)(DataPort & 0x80);
+#else
+ return 0;
+#endif
+ }
+/*------------------------------------------------
+              写入命令函数
+------------------------------------------------*/
+ void LCD_Write_Com(unsigned char com) 
+ {  
+ while(LCD_Check_Busy()); //忙则等待
+ RS_CLR; 
+ RW_CLR; 
+ EN_SET; 
+ DataPort= com; 
+ _nop_(); 
+ EN_CLR;
+ }
+/*------------------------------------------------
+              写入数据函数
+------------------------------------------------*/
+ void LCD_Write_Data(unsigned char Data) 
+ { 
+ while(LCD_Check_Busy()); //忙则等待
+ RS_SET; 
+ RW_CLR; 
+ EN_SET; 
+ DataPort= Data; 
+ _nop_();
+ EN_CLR;
+ }
+
+/*------------------------------------------------
+                清屏函数
+------------------------------------------------*/
+ void LCD_Clear(void) 
+ { 
+ LCD_Write_Com(0x01); 
+ DelayMs(5);
+ }
+/*------------------------------------------------
+              写入字符串函数
+------------------------------------------------*/
+ void LCD_Write_String(unsigned char x,unsigned char y,unsigned char *s) 
+ {     
+       
+ while (*s) 
+ 	{     
+ LCD_Write_Char(x,y,*s);     
+ s ++;  x++;   
+ 	}
+ }
+/*------------------------------------------------
+              写入字符函数
+------------------------------------------------*/
+void LCD_Write_Char(unsigned char x,unsigned char y,unsigned char Data) 
+ {     
+ if (y == 0) 
+ 	{     
+ 	LCD_Write_Com(0x80 + x);     
+ 	}    
+ else 
+ 	{     
+ 	LCD_Write_Com(0xC0 + x);     
+ 	}        
+ LCD_Write_Data( Data);  
+ }
+/*------------------------------------------------
+              初始化函数
+------------------------------------------------*/
+ void LCD_Init(void) 
+ {
+   LCD_Write_Com(0x38);    /*显示模式设置*/ 
+   DelayMs(5); 
+   LCD_Write_Com(0x38); 
+   DelayMs(5); 
+   LCD_Write_Com(0x38); 
+   DelayMs(5); 
+   LCD_Write_Com(0x38);  
+   LCD_Write_Com(0x08);    /*显示关闭*/ 
+   LCD_Write_Com(0x01);    /*显示清屏*/ 
+   LCD_Write_Com(0x06);    /*显示光标移动设置*/ 
+   DelayMs(5); 
+   LCD_Write_Com(0x0C);    /*显示开及光标设置*/
+   }
+   
+
